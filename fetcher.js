24938d117ccf7ed5568d97480e8eb5ac858789bc
Nadia Fatah <nadi.fatah@gmail.com>
2021-12-03 14:41:44 +0000

    should download the resource at the URL to the local path on your machine.

---
+++ fetcher.js
@@ -0,0 +1,28 @@
+const request = require('request'); // for HTTP request
+const fs = require('fs') // for writing to file
+const args = process.argv.slice(2); // takes cmd line arguments
+
+let urlArg = args[0];
+let pathArg = args[1];
+
+
+// HTTP request
+request(urlArg, (error, response, body) => {
+  // Write to file
+  fs.writeFile(pathArg, body, err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    // Confirmation message
+    const {size} = fs.statSync(pathArg);
+    console.log(`Downloaded and saved ${size} bytes to ./index.html`)
+  })
+});
+ 
+
+/*
+// Testing: input the following url and path in the command line
+  url = http://example.edu
+  path = /vagrant/w2/d3-net/page-fetcher/index.html
+*/
